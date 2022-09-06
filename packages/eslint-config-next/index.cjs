62704401071188c36d8a66145f8e4a8a1dea15ad
Moshe Simantov <me@moshe.io>
2022-09-06 11:33:11 +0300

    config packages

---
+++ packages/eslint-config-next/index.cjs
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-default-export
+module.exports = {
+  extends: ['next/core-web-vitals', '@neuledge'],
+
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
---
+++ packages/eslint-config/index.cjs
@@ -0,0 +1,65 @@
+// eslint-disable-next-line import/no-default-export
+module.exports = {
+  plugins: ['@typescript-eslint', 'import', 'prefer-arrow'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+
+    // Make sure this is always the last configuration in the extends array:
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-console': ['warn'],
+    'no-fallthrough': 'warn',
+    'no-warning-comments': ['warn', { terms: ['fixme'], location: 'start' }],
+    'max-lines': [
+      'warn',
+      { max: 600, skipBlankLines: true, skipComments: true },
+    ],
+    'max-lines-per-function': [
+      'warn',
+      { max: 50, skipBlankLines: true, skipComments: true },
+    ],
+    'prefer-arrow-callback': 'warn',
+    'arrow-body-style': 'warn',
+    'prefer-arrow/prefer-arrow-functions': 'warn',
+    'func-style': ['warn', 'expression', { allowArrowFunctions: true }],
+    'import/no-cycle': 'error',
+    'import/no-default-export': 'warn',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: '.',
+            message:
+              'Import from current folder is forbidden, please import from a specific file name.',
+          },
+        ],
+        patterns: [
+          {
+            group: ['../../../*'],
+            message:
+              'Relative path is too long, please use package root "@" instead.',
+          },
+          {
+            group: ['@/../../*'],
+            message:
+              'Please import package from "@neuledge" namespace instead.',
+          },
+          {
+            group: ['@neuledge/*/lib'],
+            message: 'Avoid importing the "lib" folder directly.',
+          },
+        ],
+      },
+    ],
+    '@typescript-eslint/no-empty-function': 'warn',
+  },
+};
